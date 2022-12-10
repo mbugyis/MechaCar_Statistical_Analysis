5dced424c763201c6712e238720f23cda2f5400d
mbugyis <108436137+mbugyis@users.noreply.github.com>
2022-12-09 19:09:55 -0500

    part 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,10 +12,22 @@ logit_cars <- lm(mpg ~ vehicle_weight + ground_clearance, data = mecha_cars)
 summary(logit_cars)
 
 
-# Part 2: Create Visualizations for the Trip Analysis
+# Part 2: Summary Statistics on Suspension Coils
 
 coil <- read.csv("Suspension_Coil.csv")
 
-total_summary <- coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary <- coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
-lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Part 3: T-Test on Suspension Coils
+
+t.test(coil$PSI,mu=1500)
+
+lot1 <- subset(coil, Manufacturing_Lot == "Lot1")
+lot2 <- subset(coil, Manufacturing_Lot == "Lot2")
+lot3 <- subset(coil, Manufacturing_Lot == "Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
